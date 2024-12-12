3bfa683a302599558008aeab888717754f4040ec
Ridoy1Hasan <hridoyhasaan99business@gmail.com>
2024-12-12 20:07:23 +0600

    btn added

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Noob developer</Text>
+      <Text style={ { fontSize : 40 } }>I love Shuhashini.</Text>
+      <Button title="PressHere" />
+    </View>
+  )
+}
+
+export default App
