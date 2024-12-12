3888295878d575ebae61e0989ff31ecba4dc1e37
Ridoy1Hasan <hridoyhasaan99business@gmail.com>
2024-12-12 20:48:32 +0600

    First jsx added

--- App.jsx
+++ App.jsx
@@ -1,14 +1,26 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-
+import First from './src/components/First';
 const App = () => {
   return (
     <View>
       <Text>Noob developer</Text>
-      <Text style={ { fontSize : 40 } }>I love Shuhashini.</Text>
+      <Text style={ { fontSize : 5 } }>I love Shuhashini.</Text>
       <Button title="PressHere" />
+      <First/>
+      <First/>
+      <First/>
+      <First/>
     </View>
   )
 }
+
+// const First = () =>{
+//   return(
+//     <View>
+//       <Text style={ { fontSize : 40 } }>I love Shuhashini.</Text>
+//     </View>
+//   );
+// };
 
 export default App
---
+++ src/components/First.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { View, Text, } from 'react-native'
+
+const First = () =>{
+    return(
+      <View>
+        <Text>I love Shuhashini. She is my dream girl.</Text>
+      </View>
+    );
+  };
+
+
+
+  export default First;
